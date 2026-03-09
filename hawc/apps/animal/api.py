b8898d4786a7f651e94e3b95bcb8d5c188422687
Shane Thacker <sst@clockworkmind.com>
2026-03-09 11:59:55 -0400

    Add update/delete to Animal v1 Endpoint API + PATCH serializer support
    - Add UpdateModelMixin and DestroyModelMixin to Endpoint viewset
    - Set http_method_names = METHODS_NO_PUT (PATCH only, no PUT)
    - Add perform_update/perform_destroy with logging and cache busting
    - Fix EndpointSerializer.validate to use self.instance.animal_group on update
    - Skip name/name_term requirement on partial updates not touching those fields
    - Add EndpointSerializer.update for nested groups/effects on PATCH

--- hawc/apps/animal/api.py
+++ hawc/apps/animal/api.py
@@ -11,6 +11,7 @@ from ..assessment.api import (
     BaseAssessmentViewSet,
     CleanupFieldsBaseViewSet,
     DoseUnitsViewSet,
+    METHODS_NO_PUT,
 )
 from ..assessment.constants import AssessmentViewSetPermissions
 from ..common.api.utils import get_published_only
@@ -264,10 +265,16 @@ class AnimalGroup(mixins.CreateModelMixin, AssessmentViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class Endpoint(mixins.CreateModelMixin, AssessmentViewSet):
+class Endpoint(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    AssessmentViewSet,
+):
     assessment_filter_args = "assessment"
     model = models.Endpoint
     serializer_class = serializers.EndpointSerializer
+    http_method_names = METHODS_NO_PUT
     list_actions = ["list", "effects", "rob_filter", "update_terms"]
 
     def get_queryset(self):
@@ -282,6 +289,24 @@ class Endpoint(mixins.CreateModelMixin, AssessmentViewSet):
             serializer.instance.get_assessment().id,
             self.request.user.id,
         )
+
+    @transaction.atomic
+    def perform_update(self, serializer):
+        super().perform_update(serializer)
+        create_object_log(
+            "Updated",
+            serializer.instance,
+            serializer.instance.get_assessment().id,
+            self.request.user.id,
+        )
+        serializer.instance.get_assessment().bust_cache()
+
+    @transaction.atomic
+    def perform_destroy(self, instance):
+        assessment = instance.get_assessment()
+        create_object_log("Deleted", instance, assessment.id, self.request.user.id)
+        super().perform_destroy(instance)
+        assessment.bust_cache()
 
     @action(detail=False, action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT)
     def effects(self, request):
